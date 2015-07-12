c692046a003e5641bc17d06a74faa17eb68002a9
rzgit <roger.zhang@Roger-Zhang-MacPro.local>
2015-07-12 19:54:33 +1000

    Initial Commit

---
+++ SkyTeller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkyTeller
+//
+//  Created by Roger Zhang on 12/07/2015.
+//  Copyright (c) 2015 Personal Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
